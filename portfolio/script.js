5dd7a459d59fb1ab070fdd6294d689745a003b13
ShivamJhaa <20bcs206@iiitdmj.ac.in>
2021-09-02 13:16:34 +0530

    added effects

--- portfolio/script.js
+++ portfolio/script.js
@@ -125,3 +125,24 @@ function displayProjects(projectsItems){
     displayProjects= displayProjects.join("");
     sectionCenter.innerHTML=displayProjects;
 }
+
+const navbar= document.getElementById("home");
+const topLink= document.querySelector(".top-link");
+
+window.addEventListener("scroll",function(){
+    const scrollheight= window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollheight> navHeight){
+        navbar.classList.add("fixed-nav")
+    }
+    else 
+    {
+        navbar.classList.remove("fixed-nav")
+    }
+    if (scrollheight>500){
+        topLink.classList.add('show-link')
+    }
+    else{
+        topLink.classList.remove('show-link')
+    }
+});
